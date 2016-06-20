3a8e6d8de619f5ff8a9400f178e03b1be394e9e3
Alexey Kovaliov <ikovalex@gmail.com>
2016-06-20 13:49:59 +0300

    js autocomplite <th>, modal autoenject date and time

--- js/js.js
+++ js/js.js
@@ -35,8 +35,69 @@ $(document).on('ready', function() {
             });
         });
 
+    /*----------------------Pop Up---------------------*/
+        $("td").on('click', function(){
+            var TimeData;
+            TimeData = $(this).text();
+            $("#time").attr('value',TimeData);
+            if ($(this).hasClass('firstDate')) {
+                    var DateToEnroll = $('th:first').text();
+                    $('#day').attr('value', DateToEnroll);
+                }
+            else if ($(this).hasClass('secondDate')) {
+                    var DateToEnroll = $('th:nth-child(2)').text();
+                    $('#day').attr('value', DateToEnroll);
+                }
+            else if ($(this).hasClass('thirdDate')) {
+                var DateToEnroll = $('th:nth-child(3)').text();
+                $('#day').attr('value', DateToEnroll);
+            }
+            else if ($(this).hasClass('fouthDate')) {
+                var DateToEnroll = $('th:nth-child(4)').text();
+                $('#day').attr('value', DateToEnroll);
+            }
+            else if ($(this).hasClass('fifthDate')) {
+                var DateToEnroll = $('th:nth-child(5)').text();
+                $('#day').attr('value', DateToEnroll);
+            }
+            else if ($(this).hasClass('sixDate')) {
+                var DateToEnroll = $('th:nth-child(6)').text();
+                $('#day').attr('value', DateToEnroll);
+            }
+        })
+    /*--------------------Create Table-----------------*/
+    function getWeekDay(date) {
+        var days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+        return days[date.getDay()];
+    }
+    function DateDays(date) {
+        var dd = date.getDate();
+        return dd;
+    }
+    function getMounth(date) {
+        var days = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+        return days[date.getMonth()];
+    }
 
 
+    var date = new Date();
+    function creTable() {
+            $('.table').append('<tr></tr>');
+            for(j=0;j<=5;j++) {
+                var options = {
+                    weekday: 'short',
+                    month: 'long',
+                    day: 'numeric'
+                };
+                var myDate = new Date();
+                var dayOfMonth = myDate.getDate();
+                myDate.setDate(dayOfMonth +j);
+                $('.table > tbody > tr:first').append('<th>'/*+ getWeekDay(date) +', ' + DateDays(date) +' ' + getMounth(date) +*/+ myDate.toLocaleString("ru", options) +'</th>');
+            }
+    }
+    creTable();
 
 
 
